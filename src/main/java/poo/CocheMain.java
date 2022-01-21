10d2859dc5b6945275d12e41e996bdacc53fde60
ThePowerty <w.infanzon.98@gmail.com>
2022-01-21 18:31:23 +0100

    Pedir datos

--- src/main/java/poo/CocheMain.java
+++ src/main/java/poo/CocheMain.java
@@ -1,10 +1,28 @@
 package poo;
+import java.util.Scanner;
 
 public class CocheMain {
 
+    static Scanner sc = new Scanner(System.in);
+    
     public static void main(String[] args) {
         Coche Renault = new Coche();
+        pedirDatos(Renault);
         System.out.println(Renault.getDatos());
     }
-
+    
+    public static void pedirDatos(Coche coche){
+        String color;
+        String asientos;
+        String climatizador;
+        System.out.println("Introduzca el color de su coche: ");
+        color = sc.nextLine();
+        coche.setColor(color);
+        System.out.println("¿Quiere asientos de cuero?"+"\nSi o no");
+        asientos = sc.nextLine();
+        coche.setAsientos(asientos);
+        System.out.println("¿Quiere climatizador?"+"\nSi o no");
+        climatizador = sc.nextLine();
+        coche.setClimatizador(climatizador);
+    }
 }
